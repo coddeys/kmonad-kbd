61cb9c8fe36d55f64d6614b45bf071b1ad2ca9ff
Dima S <dima@sukhikh.xyz>
2024-08-12 13:50:10 +0200

    Add corne keyboard

--- external_keyboard_script.sh
+++ external_keyboard_script.sh
@@ -4,11 +4,10 @@ set -e
 
 device_name="usb-Topre_Corporation_HHKB_Professional-event-kbd"
 dactyl="usb-tshort_handwired_dactyl_manuform_4x6_vial:f64c2b3c-event-kbd"
+corne="usb-foostan_Corne-event-if02"
 
 if [[ -e "/dev/input/by-id/${device_name}" ]]; then
-    ~/.local/bin/kmonad ~/.config/kmonad/config.kbd
-elif [[ -e "/dev/input/by-id/${dactyl}" ]]; then
-    ~/.local/bin/kmonad ~/.config/kmonad/dactyl.kbd
+    /home/dima.sukhikh/.local/bin/kmonad /home/dima.sukhikh/.config/kmonad/config.kbd
+elif [[ -e "/dev/input/by-id/${corne}" ]]; then
+    /home/dima.sukhikh/.local/bin/kmonad /home/dima.sukhikh/.config/kmonad/corne.kbd
 fi
-
-
